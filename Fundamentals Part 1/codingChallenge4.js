5b0fb03b7f0b71b0954c6851e4afac61f6d3111f
cadinkor <elliott.thomlison@gmail.com>
2021-03-22 10:22:13 +0000

    More object, map, set practice

--- Fundamentals Part 1/codingChallenge4.js
+++
@@ -1,4 +0,0 @@
-let bill = 500;
-let tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20
-
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
--- Fundamentals Part 1/script.js
+++
@@ -1,6 +0,0 @@
-console.log(10 + 5)
-
-let js = 'amazing';
-  if (js === 'amazing') alert('Javascript is FUN!');
-
-  
